472fe1f73228325556e5ca6150088ebccd28c0c5
lusferror <luisalfredorobles09@gmail.com>
2022-11-11 22:51:32 +0000

    primer commit

--- src/models.py
+++ src/models.py
@@ -1,6 +1,6 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String, Table
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
@@ -8,26 +8,41 @@ from eralchemy import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+coments_user= Table('coments_user', Base.metadata,
+    Column('coments_id',ForeignKey('coments.id'), primary_key=True),
+    Column('user_id',ForeignKey('user.id'), primary_key=True)
+)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+class User(Base):
+    __tablename__='user'
+    id= Column(Integer,primary_key= True)
+    name = Column(String)
+    user_name= Column(String)
+    email= Column(String)
+    post=relationship('post')
+    coments= relationship('coments', secondary=coments_user)
 
-    def to_dict(self):
-        return {}
+class Post(Base):
+    __tablename__='post'
+    id=Column(Integer, primary_key=True)
+    image=Column(String)
+    title= Column(String)
+    foot_photo=Column(String)
+    id_user=Column(Integer, ForeignKey('user.id'))
+    coments=relationship('Coments')
+
+
+followers = Table('followers', Base.metadata,
+    Column('id_user', ForeignKey('user.id'), primary_key=True),
+    Column('id_follower', ForeignKey('user.id'), primary_key=True)
+)
+
+class Coments(Base):
+    __tablename__='coments'
+    id= Column(Integer, primary_key=True)
+    coment=Column(String)
+    id_post=Column(Integer,ForeignKey('post.id'))
+
 
 ## Draw from SQLAlchemy base
 try:
